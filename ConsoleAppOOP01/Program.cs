bd68ef5169569ff7ca229f168d8c4cd8b5b1a0be
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-02-18 22:30:57 +0200

    Question 03

--- ConsoleAppOOP01/Program.cs
+++ ConsoleAppOOP01/Program.cs
@@ -56,10 +56,35 @@
             //}
 
 
-        #endregion
+            #endregion
 
 
-    }
+            #region Question 03
+
+
+            /* Q3: Steps to Create a Class Library
+
+                Open Visual Studio
+
+                Click Create New Project
+
+                Choose Class Library
+
+                Write your classes.
+
+                Build the project.
+
+                Add reference to it from another project:
+
+                Right click Dependencies
+
+                Add Project Reference
+
+                Select the library. */
+            #endregion
+
+
+        }
 
 
 
