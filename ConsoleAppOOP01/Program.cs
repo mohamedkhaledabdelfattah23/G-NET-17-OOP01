fac397722e933eb46ca0472a2ec72a45583ee740
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-02-18 22:23:55 +0200

    Question 01

--- ConsoleAppOOP01/Program.cs
+++ ConsoleAppOOP01/Program.cs
@@ -4,9 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            // PART 01
+
+
+            #region Question 01
+
+            /* Class → Reference Type (stored in heap, passed by reference).
+
+               Struct → Value Type (stored in stack, copied when assigned). */
+
+
+            //Person c1 = new Person();
+            //c1.Name = "Ali";
+
+            //Person c2 = c1;
+            //c2.Name = "Omar";
+
+            //Console.WriteLine(c1.Name); // Omar (same reference)
+
+
+            //PersonStr s1;
+            //s1.Name = "Ali";
+
+            //PersonStr s2 = s1;
+            //s2.Name = "Omar";
+
+            //Console.WriteLine(s1.Name); // Ali (copied value)
+
+
+            #endregion
 
 
         }
+
+
+
+
     }
+
+    #region Question 012
+    //public class Person
+    // {
+    //     public string Name;
+    // }
+
+    // public struct PersonStr
+    // {
+    //     public string Name;
+    // }
+
+    #endregion
 }
